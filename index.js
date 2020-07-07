09f691e00c575b340bea6fa0845bdb06bbb3472e
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-07-07 22:13:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const testVar = {};
+
+function testFunc() {
+	return 'hi';
+}
+
+const record = [ { year: '2018', result: 'L' }, { year: '2017', result: 'W' }, { year: '2016', result: 'N/A' } ];
+
+function superbowlWin(record) {
+	let result = record.find((record) => record.result === 'W');
+	if (result) {
+		return result.year;
+	} else {
+		return undefined;
+	}
+}
